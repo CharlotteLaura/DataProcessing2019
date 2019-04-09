6ede07705ba9f20958d5284fb3cc2f0c4aad0397
Charlotte Marks <charlottemarks@wcw-stud-145-109-39-70.wireless.uva.nl>
2019-04-09 13:07:19 +0200

    Bestanden verhuisd naar Week_1 map

---
+++ Week_1/visualizer.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# Name: Charlotte
+# Student number:
+"""
+This script visualizes data obtained from a .csv file
+"""
+
+import csv
+import matplotlib.pyplot as plt
+
+# Global constants for the input file, first and last year
+INPUT_CSV = "movies.csv"
+START_YEAR = 2008
+END_YEAR = 2018
+
+# Global dictionary for the data
+data_dict = {str(key): [] for key in range(START_YEAR, END_YEAR)}
+
+with open(INPUT_CSV, newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+       data_dict[row['Year']].append(row['Rating'])
+
+data_dict_averaged = {}
+for key, value in data_dict.items():
+    sum_value = 0
+    for s in value:
+        sum_value += float(s)
+    data_dict_averaged[int(key)] = sum_value/len(value)
+
+x = data_dict_averaged.keys()
+y = data_dict_averaged.values()
+
+plt.plot(x, y)
+plt.show()
+
+#if __name__ == "__main__":
+    #print(data_dict)
